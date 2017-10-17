5501c306daf8f4b631d98aa646d467f53c329714
Wagner Fusca <wagnerfusca@gmail.com>
2017-10-17 02:59:02 -0200

    adicionado o cadastro de cidade

--- src/main/java/br/com/db1/model/Uf.java
+++ src/main/java/br/com/db1/model/Uf.java
@@ -1,5 +1,6 @@
 package br.com.db1.model;
 
+import java.io.Serializable;
 import java.util.List;
 
 import javax.persistence.Column;
@@ -12,19 +13,18 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "uf", schema="public")
-public class Uf {
-	
+@Table(name = "uf", schema = "public")
+public class Uf implements Serializable {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Column(length=2, nullable=false)
+	@Column(length = 2, nullable = false)
 	private String nome;
-	
-	@OneToMany(fetch=FetchType.LAZY , mappedBy="uf")
+
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "uf")
 	private List<Cidade> cidades;
-	
 
 	public List<Cidade> getCidades() {
 		return cidades;
@@ -49,7 +49,36 @@ public class Uf {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Uf other = (Uf) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
 
 }
